d1c1e9545fc22faf2679c6cdda69bb41d3d33dd8
KoS <kos@KoSdeMacBook-Pro.local>
2021-08-23 15:58:06 +0800

    starting some requests

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
 
-r = requests.get('https://api.github.com/events')
 
-response = r.json()
+# The following link refers to the policy broker's URL
 
-print(response)
+payload = {"subject": "programA", "object": "fileA", "action": "read"}
+
+r_root = requests.get('http://127.0.0.1:8000/').json()
+print(r_root)
+
+r = requests.post('http://127.0.0.1:8000/check', json=payload)
+print(r.json())
